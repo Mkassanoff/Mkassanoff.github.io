9653992ebfe19c92532c38bb5ff17cc1de65b59f
Mkassanoff <70964465+Mkassanoff@users.noreply.github.com>
2022-03-31 13:34:16 -0600

    fix
    yeah

--- js/assignment11.js
+++ js/assignment11.js
@@ -1,12 +1,12 @@
 function button1Clicked(){
   document.getElementById('button-1').classList.toggle('was-clicked');
 }
-function button2Clicked(){
-  document.getElementById('button-2').classList.toggle('was-clicked2');
+function button2Mouseover(){
+  document.getElementById('button-2').classList.toggle('was-mouseover');
 }
 function windowLoaded(){
 console.log('loaded');
 document.getElementById('button-1').addEventListener('click', button1Clicked);
-document.getElementById('button-2').addEventListener('click', button2Clicked);
+document.getElementById('button-2').addEventListener('mouseover', button2Mouseover);
 }
 window.onload = windowLoaded;
