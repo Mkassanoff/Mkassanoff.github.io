0a5e4085c6abf3ca0fddc413cf3ea28a00bba208
Mkassanoff <70964465+Mkassanoff@users.noreply.github.com>
2022-04-05 10:27:46 -0600

    upload?
    weird

---
+++ week12-demo.js
@@ -0,0 +1,21 @@
+
+function getQuote() {
+console.log("quote button was clicked");
+try{
+   const response = await fetch(endpoint);
+   if(!response.ok) {
+     throw Error(response.statusText)
+   }
+
+   const json = await response.json();
+   console.log(json);
+} catch(err){
+  console.log(err);
+  alert('Failed');
+}
+}
+
+const endpoint = 'https://random-quote-generator.herokuapp.com/api/quotes/random';
+
+const quoteButton = document.querySelector('#js-new-quote');
+quoteButton.addEventListener('click', getQuote);
