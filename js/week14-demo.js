22d944ece7f563684e60a8153b0e8304768d6ee4
Mkassanoff <70964465+Mkassanoff@users.noreply.github.com>
2022-04-19 10:01:09 -0600

    changes
    y

--- js/week14-demo.js
+++ js/week14-demo.js
@@ -7,3 +7,14 @@ myCar.year = "2017";
 //general syntax: object.property = value;
 
 console.log(myCar.make);
+
+//A second way of defining an object
+const mySecondCar = {
+  color: 'Blue',
+  make: 'Ford',
+  model: 'Ranger',
+  year: '2020',
+
+}
+
+console.log(mySecondCar.make);
