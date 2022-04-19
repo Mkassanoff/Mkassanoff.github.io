645f691eabd6a2cdbe21c052d462a3e3419026e2
Mkassanoff <70964465+Mkassanoff@users.noreply.github.com>
2022-04-19 09:59:04 -0600

    update
    yup

---
+++ js/week14-demo.js
@@ -0,0 +1,9 @@
+const myCar = new Object();
+myCar.color = 'Gray';
+myCar.make = 'Mazda';
+myCar.model = "CX-5";
+myCar.year = "2017";
+
+//general syntax: object.property = value;
+
+console.log(myCar.make);
