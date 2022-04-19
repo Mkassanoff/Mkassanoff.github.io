dee660b80ae80373d397ed8afddff4e6764c74ea
Mkassanoff <70964465+Mkassanoff@users.noreply.github.com>
2022-04-19 10:26:36 -0600

    new heros
    heros

--- js/week14-demo.js
+++ js/week14-demo.js
@@ -19,3 +19,58 @@ const mySecondCar = {
 
 console.log(mySecondCar.drive);
 console.log(myCar['make']);
+
+{
+  "squadName": "Super hero squad",
+  "homeTown": "Metro City",
+  "formed": 2016,
+  "secretBase": "Super tower",
+  "active": true,
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
+Function populateHeader(x){
+Const header = document.querySelector('header');
+Const myH1 = document.querySelector('h1');
+
+myH1.textContent= x['squadName'];
+header.append.Child(myH1);
+
+const myPara = document.createElement('p');
+
+myPara.textContent = x['hometown'] x['formed'];
+header.append.Child(myPara);
+}
+populateHeader(superheros);
