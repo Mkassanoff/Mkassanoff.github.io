73788a6c877bef1933f85138d0daa78b3c1d2d00
Mkassanoff <70964465+Mkassanoff@users.noreply.github.com>
2022-04-29 14:24:53 -0600

    so much new stuff
    yay!!!

---
+++ js/dice.js
@@ -0,0 +1,21 @@
+//add d20 (random val at btn press)
+//generate html w current structure
+//step 1
+// <div class="die">
+//   <img src="img/d20.png" class="d20" alt="d20" />
+//   <span class="die-val">7</span>
+// </div>
+//
+//
+// let newDie = `
+//  <div class="die">
+//    <img src="img/d20.png" class="d20" alt="d20" />
+//    <span class="die-val">${Math.floor(Math.random() * 20) + 1}</span>
+//  </div>
+// `;
+// boxElement.innerHTML += newDie;
+
+//step2
+//query box for all DiceRoll
+//randomize val
+// = rand   no +=
