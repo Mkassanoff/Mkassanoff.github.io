0704a89b0b69277ccbfca9a61076b1ca18a6d066
Mkassanoff <70964465+Mkassanoff@users.noreply.github.com>
2022-04-30 16:13:17 -0600

    they are rolling!
    yay!

--- js/dice.js
+++ js/dice.js
@@ -19,3 +19,53 @@
 //query box for all DiceRoll
 //randomize val
 // = rand   no +=
+
+// let rollButton = document.getElementById('roller')
+// rollButton.addEventListener('click', roll)
+
+
+function roll() {
+  //1st class
+  var d100 = document.getElementsByClassName("die-val100");
+  for (var i = 0; i < d100.length; i++) {
+    let number = Math.floor(Math.random() * 100) + 1;
+    d100[i].innerHTML = number;
+    console.log(number);
+  }
+  var d20 = document.getElementsByClassName("die-val20");
+  for (var i = 0; i < d20.length; i++) {
+    let number = Math.floor(Math.random() * 20) + 1;
+    d20[i].innerHTML = number;
+    console.log(number);
+  }
+  var d12 = document.getElementsByClassName("die-val12");
+  for (var i = 0; i < d12.length; i++) {
+    let number = Math.floor(Math.random() * 12) + 1;
+    d12[i].innerHTML = number;
+    console.log(number);
+  }
+  var d10 = document.getElementsByClassName("die-val10");
+  for (var i = 0; i < d10.length; i++) {
+    let number = Math.floor(Math.random() * 10) + 1;
+    d10[i].innerHTML = number;
+    console.log(number);
+  }
+  var d8 = document.getElementsByClassName("die-val8");
+  for (var i = 0; i < d8.length; i++) {
+    let number = Math.floor(Math.random() * 8) + 1;
+    d8[i].innerHTML = number;
+    console.log(number);
+  }
+  var d6 = document.getElementsByClassName("die-val6");
+  for (var i = 0; i < d6.length; i++) {
+    let number = Math.floor(Math.random() * 6) + 1;
+    d6[i].innerHTML = number;
+    console.log(number);
+  }
+  var d4 = document.getElementsByClassName("die-val4");
+  for (var i = 0; i < d4.length; i++) {
+    let number = Math.floor(Math.random() * 4) + 1;
+    d4[i].innerHTML = number;
+    console.log(number);
+  }
+}
